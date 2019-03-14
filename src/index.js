ffc6cf64e7909ac997ef7b9031e22918440246fd
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-03-14 16:54:58 -0500

    made changes

--- src/index.js
+++ src/index.js
@@ -9,12 +9,28 @@ sayHello('World');
  */
 const {getMovies} = require('./api.js');
 
+
+
 getMovies().then((movies) => {
+
   console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+
+  let html = `<div class="row">`;
+
+  movies.forEach(({title, rating, id, img}) => {
+
+    console.log(img);
+
+    html += `<p class='col-sm-6 col-3-md movies' style='background-image: url(' + img + ")'  >` + `id#${id} - ${title} - rating: ${rating}` + "</p>";
+
   });
+
+  $(".container").html(html + "</div>");
+
 }).catch((error) => {
+
   alert('Oh no! Something went wrong.\nCheck the console for details.')
+
   console.log(error);
+
 });
